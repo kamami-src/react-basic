44ab23665c98e35e309bdb858fdafd2a5a0c18ea
kamami-src <1484miyuki@gmail.com>
2021-08-06 15:32:08 +0900

    useEffect cleanup practice

---
+++ src/components/Timer.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
+const Timer = () => {
+    const [count, setCount] = useState(0);
+    const time = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+    useEffect(() => {
+        console.log("start count")
+        const interval = setInterval(time, 1000)
+        // cleanup (Timerコンポーネントが非表示になったとき実行)
+        return () => {
+            clearInterval(interval);
+            console.log("cleared")
+        }
+    }, [])
+    return (
+        <div>
+            {count}
+        </div>
+    )
+}
+
+export default Timer
