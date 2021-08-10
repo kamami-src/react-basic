4491e20f9c8afe771707602638197a38e331848a
kamami-src <1484miyuki@gmail.com>
2021-08-10 12:04:52 +0900

    fetch practice 2

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -3,27 +3,39 @@ import React, { useState, useEffect } from 'react'
 
 const ApiFetch = () => {
     const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
+    const [clicked, setClicked] = useState(false);
+
+    const handlerClicked = () => {
+        setClicked(!clicked);
+    }
+
     useEffect(() => {
         // axios.get("https://jsonplaceholder.typicode.com/posts")
         //     .then((res) => {
         //         console.log(res);
         //         setPosts(res.data)
         //     })
-        fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET' })
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: 'GET' })
             .then((res) => res.json())
             .then((data) => {
                 console.log(data);
                 setPosts(data)
             })
-    }, [])
+    }, [clicked])
 
     return (
         <div>
-            <ul>
+            <input type="text" value={id} onChange={(e)=>{setId(e.target.value)}} />
+            <br />
+            <button type="button" onClick={handlerClicked}>Get post</button>
+            <br />
+            {posts.title}
+            {/* <ul>
                 {posts.map((post) => (
                     <li key={post.id}>{post.title}</li>
                 ))}
-            </ul>
+            </ul> */}
         </div>
     )
 }
