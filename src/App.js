c764dbe3e7aaec4813672e63cdccfed36f8ec57f
kamami-src <1484miyuki@gmail.com>
2021-08-11 13:48:22 +0900

    useReducer/useContext practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import ApiFetch from './components/ApiFetch';
+import AppContext  from './contexts/AppContext';
+// import ApiFetch from './components/ApiFetch';
 // import Basic1 from './components/Basic1';
 // import Basic2 from './components/Basic2';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
+// import B from './components/B';
+// import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+
+import React, { useReducer } from 'react'
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+    switch(action) {
+        case 'add_1':
+            return currentState + 1;
+        case 'multiple_3':
+            return currentState * 3;
+        case 'reset':
+            return initialState;
+        default:
+            return currentState;
+    }
+}
 
 function App() {
+  const [ count, dispatch ] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic1 name="Hooks" /> */}
-        {/* <Basic2 /> */}
-        {/* <BasicUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic1 name="Hooks" /> */}
+          {/* <Basic2 /> */}
+          {/* <BasicUseEffect /> */}
+          {/* <TimerContainer /> */}
+          {/* <ApiFetch />
+          <B /> */}
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
